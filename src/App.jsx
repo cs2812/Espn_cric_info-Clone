812d7e546175d636d2acaf6dd6e9be70c161a945
amaryamunavishwakarma <vishwakarmaamar805@mail.com>
2022-06-17 15:35:13 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,28 @@ import logo from './logo.svg'
 import './App.css'
 import Home from "./pages/Home"
 function App() {
-  
+  const [darkMode, setDarkMode] = useState(false);
 
   return (
-    <div className="App">
-      <Home/>
+
+    <div className={darkMode ? "dark-mode" : "light-mode"}>
+
+
+      <div>
+        <div className="container">
+          
+          <div className="switch-checkbox">
+            <label className="switch">
+              <input type="checkbox" onChange={() => setDarkMode(!darkMode)} />
+              <span className="slider round"></span>
+            </label>
+          </div>
+          
+        </div>
+      </div>
+      <div>
+        <Home />
+      </div>
     </div>
   )
 }
