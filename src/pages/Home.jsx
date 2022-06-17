b58296e91465b1d1dfd8fbb31a213599eb3c7cf5
amaryamunavishwakarma <vishwakarmaamar805@mail.com>
2022-06-17 13:58:45 +0530

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,13 +8,7 @@ import RightSideBar from '../components/RighSidebar/RightSideBar'
 import SeriesHome from '../Routes/SeriesHome/SeriesHome'
 import Video from '../Routes/Video/Video'
 import styles from "./home.module.css"
-import ReactDOM from "react-dom/client";
-import Feature from '../Routes/feature/Feature'
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import Newshome from "../Routes/feature/Newshome"
 
 const Home = () => {
   return (
@@ -25,24 +19,8 @@ const Home = () => {
 
       </div>
       <div className={styles.homeflex} >
-        
-        
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<LeftSideBar />}>
-            </Route>
-          </Routes>
-          <Routes>
-            <Route path="/" element={<News />}>
-            </Route>
-          </Routes>
-          <Routes>
-            <Route path="/feature" element={<Feature />}>
-            </Route>
-          </Routes>
-    <RightSideBar />
-            
-        </BrowserRouter>
+        <Newshome/>
+        <RightSideBar/>
       </div>
 
       <div>
