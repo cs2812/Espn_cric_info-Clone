19a6889f949dcd2c6c4a92298d5336893414fa8d
Imran Khan <99397606+imran120198@users.noreply.github.com>
2022-06-17 11:36:49 +0530

    updated team file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,34 +1,34 @@
-import React from 'react'
-import Footer from '../components/Footer/Footer'
-import LeftSideBar from '../components/LeftSideBar/LeftSideBar'
-import LiveScore from '../components/Live/LiveScore'
-import Navbar from '../components/Navbar/Navbar'
-import News from '../components/News/News'
-import RightSideBar from '../components/RighSidebar/RightSideBar'
-import SeriesHome from '../Routes/SeriesHome/SeriesHome'
-import Video from '../Routes/Video/Video'
-import styles from "./home.module.css"
+import React from "react";
+import Footer from "../components/Footer/Footer";
+import LeftSideBar from "../components/LeftSideBar/LeftSideBar";
+import LiveScore from "../components/Live/LiveScore";
+import Navbar from "../components/Navbar/Navbar";
+import News from "../components/News/News";
+import RightSideBar from "../components/RighSidebar/RightSideBar";
+import SeriesHome from "../Routes/SeriesHome/SeriesHome";
+import Video from "../Routes/Video/Video";
+import styles from "./home.module.css";
+import Teams from "../components/Teams/Teams"
 
 const Home = () => {
   return (
     <div className={styles.container}>
-        <div>
-           <Navbar/>
-            <LiveScore/> 
-           
-        </div>
-        <div className={styles.homeflex} >
-            <LeftSideBar/>
-            <News/>
-            <RightSideBar/>
-        </div>
-        
-        <div>
-            <Footer/>
-        </div>
-      
-    </div>
-  )
-}
+      {/* <div>
+        <Navbar />
+        <LiveScore />
+      </div>
+      <div className={styles.homeflex}>
+        <LeftSideBar />
+        <News />
+        <RightSideBar />
+      </div>
 
-export default Home
+      <div>
+        <Footer />
+      </div> */}
+      <Teams/>
+    </div>
+  );
+};
+
+export default Home;
