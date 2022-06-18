c8f6d7f8aaaf126d3d0b556598800c7b5c9e71ca
amaryamunavishwakarma <vishwakarmaamar805@mail.com>
2022-06-18 10:44:13 +0530

    Update LiveScore.jsx

--- src/components/Live/LiveScore.jsx
+++ src/components/Live/LiveScore.jsx
@@ -6,6 +6,53 @@ import "slick-carousel/slick/slick-theme.css";
 
 
 
+const count=[
+  {
+Sid:1,
+Ccd:"England v Australia",
+Snm:"World Series",
+ECo:"England Win by 120 points and 4 Wickets",
+Epsl:"Live",
+EtTx:"Live in England",
+len:5
+  },
+  {
+    Sid:2,
+    Ccd:"New Zealand  v South Africa",
+    Snm:"World Series",
+    ECo:"One Batsman Was Injured during Match",
+    Epsl:"Live",
+    EtTx:"Live in Belgium",
+    len:3
+      },
+      {
+        Sid:3,
+        Ccd:"India v SriLanka",
+        Snm:"T20",
+        ECo:"Rain during Match",
+        Epsl:"Live",
+        EtTx:"Live in India",
+        len:8
+          },
+          {
+            Sid:4,
+            Ccd:"England v West Indies",
+            Snm:"World Series",
+            ECo:"West indies Win by 70 points and 5 Wickets",
+            Epsl:"Live",
+            EtTx:"Live in Australia",
+            len:2
+              },
+              {
+                Sid:5,
+                Ccd:"India v Australia",
+                Snm:"World Series",
+                ECo:"India Win by 140 points and 3 Wickets",
+                Epsl:"Live",
+                EtTx:"Live in India",
+                len:4
+                  }
+]
 
 
 
@@ -13,34 +60,6 @@ import "slick-carousel/slick/slick-theme.css";
 function LiveScore() {
 
 
-
-  const [count, setCount] = useState([]);
-  console.log("count", count)
-  // useEffect(() => {
-  //   if (count.length === 0) {
-
-
-  //     const options = {
-  //       method: 'GET',
-  //       headers: {
-  //         'X-RapidAPI-Key': 'c56554ae32mshecc5ce4686ba3b3p18e9a1jsnce6b5e35a784',
-  //         'X-RapidAPI-Host': 'livescore6.p.rapidapi.com'
-  //       }
-  //     };
-
-  //     fetch('https://livescore6.p.rapidapi.com/matches/v2/list-live?Category=cricket', options)
-  //       .then(response => response.json())
-  //       .then(response => {
-  //         console.log(response.Stages)
-  //         setCount(response.Stages)
-  //       }
-  //       )
-  //       .catch(err => console.error(err));
-  //   }
-  // }, [count])
-
-
-  console.log(count[0])
 
 
   var settings = {
@@ -60,89 +79,23 @@ function LiveScore() {
       <Slider {...settings} className={"slider-div-main"}>
         
          {
-          count.map((elem,i)=>{
+          count.map((elem)=>{
             
             return(
             <div key={elem.Sid} className="slider-div">
               <h1>Location : {elem.Ccd}</h1>
                       <h1>{elem.Snm}</h1>
-                      <h2 className='highnav'>Highlight : {elem.Events[0].ECo}</h2>
-                      <h1>{elem.Events[0].EpsL}</h1>
-                      <h1>{elem.Events[0].EtTx}</h1>
+                      <h2 className='highnav'>Highlight : {elem.ECo}</h2>
+                      <h1>{elem.EpsL}</h1>
+                      <h1>{elem.EtTx}</h1>
                       
-                      <h2>Played matches : {elem.Events.length}</h2>
-            </div>
-            )
-          })
-         }
-         {
-          count.map((elem,i)=>{
-            
-            return(
-            <div key={elem.Sid} className="slider-div">
-              <h1>Location : {elem.Ccd}</h1>
-                      <h1>{elem.Snm}</h1>
-                      <h2 className='highnav'>Highlight : {elem.Events[0].ECo}</h2>
-                      <h1>{elem.Events[0].EpsL}</h1>
-                      <h1>{elem.Events[0].EtTx}</h1>
-                      
-                      <h2>Played matches : {elem.Events.length}</h2>
-            </div>
-            )
-          })
-         }
-          {
-          count.map((elem,i)=>{
-            
-            return(
-            <div key={elem.Sid} className="slider-div">
-              <h1>Location : {elem.Ccd}</h1>
-                      <h1>{elem.Snm}</h1>
-                      <h2 className='highnav'>Highlight : {elem.Events[0].ECo}</h2>
-                      <h1>{elem.Events[0].EpsL}</h1>
-                      <h1>{elem.Events[0].EtTx}</h1>
-                      
-                      <h2>Played matches : {elem.Events.length}</h2>
-            </div>
-            )
-          })
-         }
-          {
-          count.map((elem,i)=>{
-            
-            return(
-            <div key={elem.Sid} className="slider-div">
-              <h1>Location : {elem.Ccd}</h1>
-                      <h1>{elem.Snm}</h1>
-                      <h2 className='highnav'>Highlight : {elem.Events[0].ECo}</h2>
-                      <h1>{elem.Events[0].EpsL}</h1>
-                      <h1>{elem.Events[0].EtTx}</h1>
-                      
-                      <h2>Played matches : {elem.Events.length}</h2>
-            </div>
-            )
-          })
-         }
-          {
-          count.map((elem,i)=>{
-            
-            return(
-            <div key={elem.Sid} className="slider-div">
-              <h1>Location : {elem.Ccd}</h1>
-                      <h1>{elem.Snm}</h1>
-                      <h2 className='highnav'>Highlight : {elem.Events[0].ECo}</h2>
-                      <h1>{elem.Events[0].EpsL}</h1>
-                      <h1>{elem.Events[0].EtTx}</h1>
-                      
-                      <h2>Played matches : {elem.Events.length}</h2>
+                      <h2>Played matches : {elem.len}</h2>
             </div>
             )
           })
          }
         
-         
-          
-        
+  
       </Slider>
     </div> </div>
   )
